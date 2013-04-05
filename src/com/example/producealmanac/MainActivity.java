af67acd4ceae9bc5089508eedf601e2ce587373d
Shaun <shauncbenjamin@gmail.com>
2013-04-05 16:37:00 -0700

    fake pictures and descriptions added

--- src/com/example/producealmanac/MainActivity.java
+++ src/com/example/producealmanac/MainActivity.java
@@ -8,16 +8,26 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 	
+	//currentItems will be the items currently in season, use a hashmap with key=time of year
 	ArrayList<Item> currentItems;
+	boolean populated=false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		//add all current items to the ArrayList
 		currentItems = new ArrayList<Item>();
 		currentItems.add(new Item("apple"));
+		currentItems.add(new Item("potato"));
+		currentItems.add(new Item("strawberries"));
 		//etc etc
+		
+		if(!populated){
+			populated=true;
+			Item.populateMap();
+		}
 		
 	}
 
